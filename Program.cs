291dc124f87282b972c1c6357b96b467444f3288
gveluchamy <86959937+gveluchamy@users.noreply.github.com>
2021-07-12 03:45:06 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(add(3, 7));
+            Console.WriteLine(add(3, 5));
              Console.WriteLine("this is pus");
              
 
