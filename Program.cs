c522958421aa8a779c1f153085fa50edda42449e
vpganapathy2@gmail.com <vpganapathy2@gmail.com>
2021-07-12 00:01:35 -0700

    calculate

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(add(3, 9));
+
+
+
+
+        }
+
+
+
+        public static int add(int x, int y)
+        {
+
+
+            int z = x + y;
+            return z;
+        }
+    }
+
+
+
+}
